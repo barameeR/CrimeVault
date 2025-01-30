a27e7827ef5012db53a15c88caf6f96fe2d57dc3
BARAMEE RUJIPATANAPONG <baramee.rujipatanapong@gmail.com>
2025-01-30 20:44:42 +1100

    Add Entity class with properties and equality methods
    Introduce `CrimeVault.Domain.Primitives` namespace and define an abstract `Entity` class. Implement `IEquatable<Entity>` and override `Equals` and `GetHashCode` methods. Add equality operators. Include properties: `Id`, `CreatedAt`, `UpdatedAt`, `IsDeleted`, `DeletedAt`, `DeletedBy`, `UpdatedBy`, and `CreatedBy`. Define `DeleteStatus` enum with `Active`, `Deleted`, and `Archived` values.

---
+++ CrimeVault.Domain/Primitives/Entity.cs
@@ -0,0 +1,74 @@
+﻿
+
+namespace CrimeVault.Domain.Primitives;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    override public bool Equals(object? obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+        var other = (Entity)obj;
+        return Id == other.Id;
+    }
+
+    override public int GetHashCode()
+    {
+        return HashCode.Combine(Id);
+    }
+
+    public bool Equals(Entity? other)
+    {
+        if (other == null)
+        {
+            return false;
+        }
+
+        if (other.GetType() != GetType())
+        {
+            return false;
+                };
+        return Id == other.Id;
+    }
+
+    public static bool operator ==(Entity? a, Entity? b)
+    {
+        if (a is null && b is null)
+        {
+            return true;
+        }
+        if (a is null || b is null)
+        {
+            return false;
+        }
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(Entity? a, Entity? b)
+    {
+        return !(a == b);
+    }
+
+    public Guid Id { get; private init; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public DeleteStatus IsDeleted { get; set; } = DeleteStatus.Active;
+    public DateTime? DeletedAt { get; set; }
+    public string? DeletedBy { get; set; }
+    public string? UpdatedBy { get; set; }
+    public string? CreatedBy { get; set; }
+}
+public enum DeleteStatus
+{
+    Active = 0,
+    Deleted = 1,
+    Archived = 2
+}
+
